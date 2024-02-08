ebe6636448b920a3123d231dbaf6e8dc3310cb51
piotr piekos <piotrpiekos@gmail.com>
2024-02-08 10:53:16 +0300

    adds copy to ibex to the repo

---
+++ copy_to_ibex.sh
@@ -0,0 +1,4 @@
+scp -r train-scripts/*.py piekosp@glogin.ibex.kaust.edu.sa:/home/piekosp/cs325_project/erasing_concepts/train-scripts/
+scp -r eval-scripts/*.py piekosp@glogin.ibex.kaust.edu.sa:/home/piekosp/cs325_project/erasing_concepts/eval-scripts/
+scp data/* piekosp@glogin.ibex.kaust.edu.sa:/home/piekosp/cs325_project/erasing_concepts/data
+scp -r results piekosp@glogin.ibex.kaust.edu.sa:/home/piekosp/cs325_project/erasing_concepts/
